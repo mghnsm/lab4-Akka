4cceffcc3f1cf401c60a34e534905e9d87675c80
Anna <mghnsm@gmail.com>
2020-12-11 23:10:37 +0300

    Scripted auto-commit on change (2020-12-11 23:10:37) by gitwatch.sh

--- src/main/java/bmstu/TestData.java
+++ src/main/java/bmstu/TestData.java
@@ -3,10 +3,14 @@ package bmstu;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public class TestData {
-    
+    private TestPackage parent;
+    private String name;
+    private String result;
+    private String expected;
 
     @JsonCreator
-    public TestData() {
-
+    public TestData(String name, String expected) {
+        this.name = name;
+        this.expected = expected;
     }
 }
