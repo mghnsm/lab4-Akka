377902d9e535738beaf4dacca2c18f6a44a163a0
Anna <mghnsm@gmail.com>
2020-12-11 21:38:24 +0300

    Scripted auto-commit on change (2020-12-11 21:38:24) by gitwatch.sh

--- src/main/java/bmstu/Akka.java
+++ src/main/java/bmstu/Akka.java
@@ -1,7 +1,14 @@
 package bmstu;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
 public class Akka {
     public static void main(String[] args) throws Exception {
-        
+        ActorSystem system = ActorSystem.create("Akka");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
     }
 }
