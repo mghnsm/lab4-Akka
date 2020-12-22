29ef32f7ec7cf36907fa8067d2fcf9d4116c4568
Anna <mghnsm@gmail.com>
2020-12-22 04:12:34 +0300

    Scripted auto-commit on change (2020-12-22 04:12:34) by gitwatch.sh

--- src/main/java/bmstu/RouterActor.java
+++ src/main/java/bmstu/RouterActor.java
@@ -1,10 +1,11 @@
 package bmstu;
 
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.SupervisorStrategy;
 import akka.actor.dsl.Creators;
 
-public class RouterActor {
+public class RouterActor implements AbstractActor {
     private ActorRef storageActor;
     private SupervisorStrategy strategy;
     private ActorRef testerActor;
