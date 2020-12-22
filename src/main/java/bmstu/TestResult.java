0ce66efc4b0a6b66e502c72b4395de10ba632a13
Anna <mghnsm@gmail.com>
2020-12-22 03:28:10 +0300

    Scripted auto-commit on change (2020-12-22 03:28:10) by gitwatch.sh

--- src/main/java/bmstu/TestResult.java
+++ src/main/java/bmstu/TestResult.java
@@ -1,9 +1,16 @@
 package bmstu;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class TestResult {
     private String result;
     private String expected;
     private Boolean success;
 
-    
+    @JsonCreator
+    public TestResult(String result, String expected, Boolean success) {
+        this.result = result;
+        this.expected = expected;
+        this.success = success;
+    }
 }
