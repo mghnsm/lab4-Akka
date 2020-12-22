38bfc5f885d6192bd7a72b5b70f73da2461ef515
Anna <mghnsm@gmail.com>
2020-12-22 03:45:40 +0300

    Scripted auto-commit on change (2020-12-22 03:45:40) by gitwatch.sh

--- src/main/java/bmstu/TesterActor.java
+++ src/main/java/bmstu/TesterActor.java
@@ -1,5 +1,11 @@
 package bmstu;
 
+import akka.actor.ActorRef;
+
 public class TesterActor {
-    
+    private ActorRef storageActor;
+
+    TesterActor(ActorRef storageActor) {
+        this.storageActor = storageActor;
+    }
 }
