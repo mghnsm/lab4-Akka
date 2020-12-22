8e3101e126854f20fb9cd61b8d70239d09a08ac8
Anna <mghnsm@gmail.com>
2020-12-22 03:34:38 +0300

    Scripted auto-commit on change (2020-12-22 03:34:38) by gitwatch.sh

--- src/main/java/bmstu/StorageActor.java
+++ src/main/java/bmstu/StorageActor.java
@@ -1,4 +1,10 @@
 package bmstu;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class StorageActor {
+    private Map<String, ArrayList<TestData>> storage = new HashMap<>();
+    
 }
