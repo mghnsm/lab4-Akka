7b1d56ca688550a9408f664c4fcb62137c1c76d1
Anna <mghnsm@gmail.com>
2020-12-22 04:04:42 +0300

    Scripted auto-commit on change (2020-12-22 04:04:42) by gitwatch.sh

--- src/main/java/bmstu/Request.java
+++ src/main/java/bmstu/Request.java
@@ -1,10 +1,16 @@
 package bmstu;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import java.util.List;
 
 public class Request {
     private String packageId;
     private List<TestResult> tests;
 
-    
+    @JsonCreator
+    public Request(String packageId, List<TestResult> tests) {
+        this.packageId = packageId;
+        this.tests = tests;
+    }
 }
