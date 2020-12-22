6bcda6e4d729b822ea91c0a400d79255fb548a0e
Anna <mghnsm@gmail.com>
2020-12-22 17:00:54 +0300

    Scripted auto-commit on change (2020-12-22 17:00:54) by gitwatch.sh

--- src/main/java/bmstu/TestPackage.java
+++ src/main/java/bmstu/TestPackage.java
@@ -1,7 +1,6 @@
 package bmstu;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 import java.util.ArrayList;
