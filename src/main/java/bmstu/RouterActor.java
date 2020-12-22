bc5948cba24ce01e44b9a7f26486d29a9c73ae8d
Anna <mghnsm@gmail.com>
2020-12-22 16:18:39 +0300

    Scripted auto-commit on change (2020-12-22 16:18:39) by gitwatch.sh

--- src/main/java/bmstu/RouterActor.java
+++ src/main/java/bmstu/RouterActor.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 
 public class RouterActor extends AbstractActor {
     public static final int MAX_NR_OF_RETRIES = 4;
+    public static final int WORKERS_COUNT = 5;
     private ActorRef storageActor;
     private SupervisorStrategy strategy;
     private ActorRef testerActor;
@@ -18,7 +19,7 @@ public class RouterActor extends AbstractActor {
     RouterActor(ActorSystem sys) {
         this.storageActor = sys.actorOf(Props.create(StorageActor.class), "StorageActor");
         this.strategy = new OneForOneStrategy(MAX_NR_OF_RETRIES, Duration.ofMinutes(1), Collections.singletonList(Exception.class));
-        this.testerActor = sys.actorOf(new RoundRobinPool(5).withSupervisorStrategy(strategy).props(Props.create(TesterActor.class, storageActor)));
+        this.testerActor = sys.actorOf(new RoundRobinPool(WORKERS_COUNT).withSupervisorStrategy(strategy).props(Props.create(TesterActor.class, storageActor)));
     }
 
     public void runTests(TestPackage testPackage) {
